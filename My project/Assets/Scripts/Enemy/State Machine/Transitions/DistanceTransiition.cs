6dfd7f6ef8ddb0ba6f45304e5b0e3d7e85feea9f
AlexVin123 <lexuz1998@gmail.com>
2022-12-05 14:54:33 +0300

    v002

---
+++ My project/Assets/Scripts/Enemy/State Machine/Transitions/DistanceTransiition.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistanceTransiition : Transition
+{
+    [SerializeField] private float _transitonRange;
+    [SerializeField] private float _rangetSpreat;
+
+    private void Start()
+    {
+        _transitonRange += Random.Range(-_rangetSpreat, _rangetSpreat);
+    }
+
+    private void Update()
+    {
+        if(Vector2.Distance(transform.position, Target.transform.position) < _transitonRange)
+        {
+            NeedTransit = true;
+        }
+    }
+}
+
